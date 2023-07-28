08db247d68515f37fb370782bc4de79661eb061b
TheVisual <132447890+TheVisual@users.noreply.github.com>
2023-07-28 10:11:45 -0500

    Update HttpClientExtensions.cs

--- WebShare Account Creator/Extensions/HttpClientExtensions.cs
+++ WebShare Account Creator/Extensions/HttpClientExtensions.cs
@@ -8,16 +8,4 @@ public static class HttpClientExtensions
         var jsonPayload = new StringContent(JsonSerializer.Serialize(content), Encoding.UTF8, "application/json");
         return await httpClient.PostAsync(requestUri, jsonPayload);
     }
-
-    public static async Task<Dictionary<string, object>?> GetJsonAsync(this HttpClient httpClient, string requestUri)
-    {
-        var response = await httpClient.GetAsync(requestUri);
-        response.EnsureSuccessStatusCode();
-
-        var respBody = await response.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<Dictionary<string, object>?>(
-            respBody,
-            new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
-        );
-    }
 }
